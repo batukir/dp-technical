36621ba70ef68464ac8014acc211127a6d1f053b
BatuhanKir <47462383+BatuhanKir@users.noreply.github.com>
2021-11-16 13:59:59 -0500

    health-checkboxes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d537b682d058588c843";
+/******/ 	var hotCurrentHash = "c1fe86a6796cbb5a32b7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/XMLForm/index.tsx
+++ components/XMLForm/index.tsx
@@ -1,91 +1,125 @@
-import {
-  TextField,
-  Paper,
-  Box,
-  Button,
-  FormControl,
-  InputLabel,
-  Input,
-} from '@material-ui/core';
-import { create as createXMLDoc } from 'xmlbuilder2';
-import fileDownload from 'js-file-download';
+import { TextField, Paper, Box, Button, Typography } from "@material-ui/core";
+import { create as createXMLDoc } from "xmlbuilder2";
+import fileDownload from "js-file-download";
+import React, { useState } from "react";
 
 export default function XMLForm() {
+  const health = [
+    { value: "High Blood Pressure", isChecked: false },
+    { value: "Heart Disease", isChecked: false },
+    { value: "Cholesterol", isChecked: false },
+  ];
+  const [healthCheckbox, setHealthCheckbox] = useState(health);
+  const checkedItems = healthCheckbox.filter(({ isChecked }) => isChecked);
+  var viewChecked = [];
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.elements);
-
     const { firstName, lastName, address, companyName } = e.target.elements;
-    const xmlDoc = createXMLDoc({ version: '1.0' })
-      .ele('user')
-      .ele('firstName')
+    checkedItems.forEach((e) => {
+      viewChecked.push(e.value);
+    });
+    const xmlDoc = createXMLDoc({ version: "1.0" })
+      .ele("user")
+      .ele("firstName")
       .txt(firstName.value)
       .up()
-      .ele('lastName')
+      .ele("lastName")
       .txt(lastName.value)
       .up()
-      .ele('address')
+      .ele("address")
       .txt(address.value)
       .up()
-      .ele('companyName')
+      .ele("companyName")
       .txt(companyName.value)
       .up()
+      .ele("healthInfo")
+      .txt(viewChecked.toString())
       .up();
     const xmlFile = xmlDoc.end({ prettyPrint: true });
-    fileDownload(xmlFile, 'xml-form.xml');
+    fileDownload(xmlFile, "xml-form.xml");
   };
 
   return (
     <Paper elevation={5} style={{ marginTop: 60, padding: 30 }}>
-      <img src='/DataPassportsLogo.svg' style={{ marginLeft: 15 }} />
-      <Box display='flex' flexDirection='column' style={{ margin: 40 }}>
-        <form id='xml-form' onSubmit={handleSubmit}>
+      <img src="/DataPassportsLogo.svg" style={{ marginLeft: 15 }} />
+      <Box display="flex" flexDirection="column" style={{ margin: 20 }}>
+        <form id="xml-form" onSubmit={handleSubmit}>
           <div>
             <TextField
-              id='firstName'
-              inputProps={{ 'data-testid': 'required-firstName'}}
+              id="firstName"
+              inputProps={{ "data-testid": "required-firstName" }}
               required
-              label='First Name'
+              label="First Name"
               style={{ marginBottom: 15 }}
             />
           </div>
           <div>
             <TextField
-              id='lastName'
-              inputProps={{ 'data-testid': 'required-lastName'}}
+              id="lastName"
+              inputProps={{ "data-testid": "required-lastName" }}
               required
-              label='Last Name'
+              label="Last Name"
               style={{ marginBottom: 15 }}
             />
-          </div>{' '}
+          </div>{" "}
           <div>
             <TextField
-              id='address'
-              inputProps={{ 'data-testid': 'required-address'}}
+              id="address"
+              inputProps={{ "data-testid": "required-address" }}
               required
-              label='Address'
+              label="Address"
               style={{ marginBottom: 15 }}
             />
-          </div>{' '}
+          </div>{" "}
           <div>
             <TextField
-              id='companyName'
-              inputProps={{ 'data-testid': 'required-companyName'}}
+              id="companyName"
+              inputProps={{ "data-testid": "required-companyName" }}
               required
-              label='Company Name'
+              label="Company Name"
               style={{ marginBottom: 15 }}
             />
           </div>
+          <Typography variant="h6">
+            Please select all health issues that apply:
+          </Typography>
+          <ul className="list-group" style={{ marginBottom: 15 }}>
+            {healthCheckbox.map((checkbox, index) => (
+              <li className="list-group-item rounded-0" key={checkbox.value}>
+                <div className="custom-control custom-checkbox">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="healthInfo"
+                    onChange={(e) => {
+                      const newCheckboxes = [...healthCheckbox];
+                      newCheckboxes[index].isChecked = e.target.checked;
+                      setHealthCheckbox(newCheckboxes);
+                    }}
+                    checked={checkbox.isChecked}
+                  />
+                  <label
+                    className="cursor-pointer font-italic d-block custom-control-label"
+                    htmlFor={checkbox.value}
+                  >
+                    {checkbox.value}
+                  </label>
+                </div>
+              </li>
+            ))}
+          </ul>
           <Button
-            type='submit'
-            form='xml-form'
-            color='primary'
-            variant='contained'
+            type="submit"
+            form="xml-form"
+            color="primary"
+            variant="contained"
             style={{ marginBottom: 15 }}
           >
             Generate XML
           </Button>
-        </form>{' '}
+        </form>{" "}
       </Box>
     </Paper>
   );
